09f6d1725eb35b8885b97b639bda450f3af0d653
slabgames <55172860+slabgames@users.noreply.github.com>
2025-03-26 17:08:42 +0800

    Update GodotTranssion.java

--- transsionsdk/src/main/java/com/slabgames/transsionsdk/GodotTranssion.java
+++ transsionsdk/src/main/java/com/slabgames/transsionsdk/GodotTranssion.java
@@ -1,10 +1,50 @@
 package com.slabgames.transsionsdk;
 
 
+import android.app.Activity;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import org.godotengine.godot.Godot;
 import org.godotengine.godot.plugin.GodotPlugin;
 import org.godotengine.godot.plugin.SignalInfo;
 import org.godotengine.godot.plugin.UsedByGodot;
 
-public class GodotTranssion {
+import com.transsion.gamead.AdInitializer;
+
+public class GodotTranssion extends GodotPlugin  {
+    public GodotTranssion(Godot godot) {
+        super(godot);
+    }
+
+    @NonNull
+    @Override
+    public String getPluginName() {
+        return "GodotTranssion";
+    }
+
+    @Nullable
+    @Override
+    public View onMainCreate(Activity activity) {
+
+        AdInitializer.init(
+                new AdInitializer.Builder(this)
+                        //Set setDebuggable to true, which indicates that logging is enabled. Set it to false when the app is realesed in a formal environment.
+                        //Even if it is set to false, you can still use the following adb command to enable logging on your debugging device to facilitate debugging and verification.
+                        //adb shell setprop log.tag.GameAdLog DEBUG
+
+                        .setDebuggable(false)
+                        /*Set the running environment, test indicates a test environment. Set the value to release for an online environment.*/
+                        .setEnv("release")
+                        //Set test to display the testing server advertisement of AdMob.
+                        //If you want to debug real online advertisements, set setEnv to release, enter the device ID, and call setTestDeviceIds.
+                        //Refer to FAQs for obtaining the testing device ID. Please delete this line in an official online environment.
+                        //.setTestDeviceIds(Collections.singletonList("7FC2C0BE39C47406C984C08C16418C5C"))
+                        //Advertising switch. It is enabled by default if not specified.
+                        .setTotalSwitch(true)
+        );
+        return super.onMainCreate(activity);
+    }
 }
